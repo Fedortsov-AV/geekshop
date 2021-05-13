00aef78d1a274a01d4f499ca779978441fc6383e
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2021-05-14 00:48:40 +1000

    Added basket_remove controller

--- basketapp/views.py
+++ basketapp/views.py
@@ -16,3 +16,7 @@ def basket_add(request, product_id=None):
         basket.quantity += 1
         basket.save()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def basket_remove (request, id):
+    basket = Basket.objects.get(id=id).delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
