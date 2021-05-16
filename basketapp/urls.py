4c73fd7241954debe5fa8a37b6966bc5eda78e76
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2021-05-16 15:35:04 +1000

    Added controller basket_edit

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from basketapp.views import basket_add, basket_remove
+from basketapp.views import basket_add, basket_remove, basket_edit
 
 
 app_name = "basketapp"
@@ -7,4 +7,5 @@ app_name = "basketapp"
 urlpatterns = [
     path('add/<int:product_id>/', basket_add, name='basket_add'),
     path('remove/<int:id>/', basket_remove, name='basket_remove'),
+    path('edit/<int:id>/<int:quantity>/', basket_edit, name='basket_edit'),
 ]
--- static/js/basket.js
+++ static/js/basket.js
@@ -1,12 +1,9 @@
 window.onload = function () {
     $('.basket_list').on('click', 'input[type ="number"]', function () {
         var t_href = event.target;
-        // console.log(t_href.name);
-        // console.log(t_href.value);
         $.ajax({
-            url: "/baskets/edit" + t_href.name + "/" + t_href.value + "/",
+            url: "/baskets/edit/" + t_href.name + "/" + t_href.value + "/",
             success: function (data) {
-                console.log(data);
                 $('.basket_list').html(data.result);
             }
         });
