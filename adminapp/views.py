822ce5521ef726245330c0081df25953e93227f3
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2021-05-19 23:58:41 +1000

    added users_read

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from authapp.models import User
 
 def index(request):
     return render(request, 'adminapp/admin.html')
+
+
 # CRUD - Create Read Update Delete
+
+def admin_users_read(request):
+    context = {'users': User.objects.all()}
+    return render(request, 'adminapp/admin-users-read.html', context)
