a4fdbfc51907da39bdc1026b33db5f357093c9ac
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2021-05-20 18:39:12 +1000

    added controller for restore user

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from adminapp.views import index, admin_users_read, admin_users_create, admin_users_update, admin_user_remove
-
+from adminapp.views import index, admin_users_read, admin_users_create, admin_users_update, admin_user_remove, \
+    admin_user_restore
 
 app_name = "adminapp"
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('admin-users-create/', admin_users_create, name='admin_users_create'),
     path('admin-users-update/<int:user_id>/', admin_users_update, name='admin_users_update'),
     path('admin-user-remove/<int:user_id>/', admin_user_remove, name='admin_user_remove'),
+    path('admin-user-restore/<int:user_id>/', admin_user_restore, name='admin_user_restore'),
 
-   ]
+]
--- adminapp/views.py
+++ adminapp/views.py
@@ -53,3 +53,11 @@ def admin_user_remove(request, user_id):
     user.is_active = False
     user.save()
     return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_user_restore(request, user_id):
+    user = User.objects.get(id=user_id)
+    user.is_active = True
+    user.save()
+    return HttpResponseRedirect(reverse('admin_staff:admin_users_read'))
