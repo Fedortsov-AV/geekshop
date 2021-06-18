577437a5a2f6b4dc16781031a9ade2aeaf944cb8
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2021-06-18 22:59:25 +1000

    Добавлен контекстный процессор

---
+++ mainapp/context_processors.py
@@ -0,0 +1,9 @@
+from mainapp.models import ProductCategory
+
+
+def category(request):
+    category_list = []
+    category_list = ProductCategory.objects.all()
+    return {
+        'category': category_list
+    }
--- mainapp/views.py
+++ mainapp/views.py
@@ -17,7 +17,6 @@ class ProductView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProductView, self).get_context_data(**kwargs)
         context.update({'title': 'GeekShop - товары'})
-        context.update({'category': ProductCategory.objects.all()})
         return context
 
 
@@ -32,5 +31,4 @@ class ProductCategoryView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProductCategoryView, self).get_context_data(**kwargs)
         context.update({'title': 'GeekShop - товары'})
-        context.update({'category': ProductCategory.objects.all()})
         return context
