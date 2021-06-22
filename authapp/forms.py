8bac81b17deacb71902e867c9000d3df6282d8bb
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2021-06-22 23:05:14 +1000

    Реализовано автозаполнение данных из профиля ВК

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,7 +1,10 @@
+import hashlib
+import random
+
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
 from django.forms import ModelForm
-from authapp.models import User
+from authapp.models import User, UserProfile
 from mainapp.models import Product
 
 
@@ -37,6 +40,14 @@ class UserRegisterForm(UserCreationForm):
         'class': 'form-control py-4', 'placeholder': 'Подтвердите пароль'
     }))
 
+    def save(self):
+        user = super().save()
+        user.is_active = False
+        salt = hashlib.sha1(str(random.random()).encode('utf8')).hexdigest()[:6]
+        user.activation_key = hashlib.sha1((user.email + salt).encode('utf-8')).hexdigest()
+        user.save()
+        return user
+
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
@@ -55,6 +66,24 @@ class UserProfileForm(UserChangeForm):
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'image')
+
+
+class UserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('tagline', 'about_me', 'gender')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
