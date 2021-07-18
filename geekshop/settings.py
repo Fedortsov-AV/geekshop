1c42ca1736261d1fe61dbfe7b6eb10c900aaba9f
Fedortsov-AV <aleksandrfedorcov431@gmail.com>
2021-07-18 20:20:42 +1000

    STATIC_ROOT

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -154,9 +154,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
 
 FIXTURE_DIRS = (
     os.path.join(BASE_DIR, 'mainapp/'),
